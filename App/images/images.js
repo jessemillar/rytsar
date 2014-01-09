d16515a4653589fb7fe2cfb7d4c62a92e2bac9a9
Jesse Millar <jesse@jessemillar.com>
2014-01-08 23:13:56 -0700

    Cleaned up images.

--- App/images/images.js
+++ App/images/images.js
@@ -1,9 +1,9 @@
-var imgZombie = new Image(75, 75)
-	imgZombie.src = 'images/zombie.png'
-var imgZombie2 = new Image(75, 75)
-	imgZombie2.src = 'images/zombie2.png'
+var imgZombie = new Image(68, 68)
+	imgZombie.src = 'images/zombie-down-right.png'
+var imgZombie2 = new Image(68, 68)
+	imgZombie2.src = 'images/zombie-down-right-2.png'
 
-var imgZombieLeft = new Image(75, 75)
-	imgZombieLeft.src = 'images/zombie-left.png'
-var imgZombieLeft2 = new Image(75, 75)
-	imgZombieLeft2.src = 'images/zombie-left2.png'
+var imgZombieLeft = new Image(68, 68)
+	imgZombieLeft.src = 'images/zombie-down-left.png'
+var imgZombieLeft2 = new Image(68, 68)
+	imgZombieLeft2.src = 'images/zombie-down-left-2.png'
