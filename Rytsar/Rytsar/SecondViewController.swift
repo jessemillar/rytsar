12512c7fee7d4bcee40556c6b1e29a6b578fd7d5
Jesse Millar <hellojessemillar@gmail.com>
2016-01-24 19:30:37 -0700

    Adding a tabbed Hello, World app

---
+++ Rytsar/Rytsar/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Rytsar
+//
+//  Created by Jesse Millar on 1/24/16.
+//  Copyright © 2016 Jesse Millar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
