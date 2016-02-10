ee84eb626fb933e7d8cf7cd4b0748ce03b39d247
Jesse Millar <hellojessemillar@gmail.com>
2016-02-10 14:14:10 -0700

    Aiming works more reliably

--- Rytsar/Rytsar/MapViewController.swift
+++ Rytsar/Rytsar/MapViewController.swift
@@ -24,7 +24,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     var enemies = [Enemy]()
     
     let enemyRadius = 2 // Kilometers
-    let shootRadius = 200.0 // In meters
+    let shootRadius = 150.0 // In meters
     
     var enemiesLoaded = false
 
@@ -43,8 +43,6 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
         
         let gunFireFile = NSBundle.mainBundle().URLForResource("fire", withExtension: "mp3")
         do {
@@ -119,13 +117,6 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         userLatitude = location!.coordinate.latitude
         userLongitude = location!.coordinate.longitude
         
-//        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
-//        
-//        let zoom = 0.0035
-//        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: zoom, longitudeDelta: zoom))
-//        
-//        self.mapView.setRegion(region, animated: true) // Zoom into the user's current location
-        
         mapView.setUserTrackingMode(MKUserTrackingMode.FollowWithHeading, animated: true)
         
         if !enemiesLoaded {
@@ -163,35 +154,26 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
                     let pin = EnemyPin(coordinate: CLLocationCoordinate2D(latitude: enemy.latitude, longitude: enemy.longitude))
                     self.mapView.addAnnotation(pin)
                 }
-                
-                for enemy in self.enemies {
-                    let angle = self.angleTo(self.userLatitude, lon1: self.userLongitude, lat2: enemy.latitude, lon2: enemy.longitude)
-                    
-                    //            if newHeading.magneticHeading > (angle - halfCone) && newHeading.magneticHeading < (angle + halfCone) && withinRange(enemy.latitude, lon1: enemy.longitude, lat2: userLatitude, lon2: userLongitude) {
-                    print(angle)
-                }
             }
             
             task.resume()
         }
-        
-//        self.locationManager.stopUpdatingLocation() // Stop updating the location so we can zoom around the map
     }
     
     func locationManager(manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
         for enemy in enemies {
-            let halfCone = 2.0
-//            let angle = angleTo(enemy.latitude, lon1: enemy.longitude, lat2: userLatitude, lon2: userLongitude)
+            let halfCone = 8.0
+            let angle = angleTo(userLatitude, lon1: userLongitude, lat2: enemy.latitude, lon2: enemy.longitude)
+            let distance = distanceTo(userLatitude, lon1: userLongitude, lat2: enemy.latitude, lon2: enemy.longitude)
             
-//            if newHeading.magneticHeading > (angle - halfCone) && newHeading.magneticHeading < (angle + halfCone) && withinRange(enemy.latitude, lon1: enemy.longitude, lat2: userLatitude, lon2: userLongitude) {
-//                print(angle)
-//                print((angle - halfCone), newHeading.magneticHeading, (angle + halfCone), withinRange(enemy.latitude, lon1: enemy.longitude, lat2: userLatitude, lon2: userLongitude))
+//            print(newHeading.magneticHeading, angle)
+            
+            if newHeading.magneticHeading > (angle - halfCone) && newHeading.magneticHeading < (angle + halfCone) && distance < shootRadius {
+//                print((angle - halfCone), newHeading.magneticHeading, (angle + halfCone), distanceTo(enemy.latitude, lon1: enemy.longitude, lat2: userLatitude, lon2: userLongitude))
                 AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-//                print("Aimed", newHeading.magneticHeading, angle, enemy.latitude, enemy.longitude)
-//            }
+                print("Aimed", newHeading.magneticHeading, angle, distance)
+            }
         }
-        
-//        print(newHeading.magneticHeading)
     }
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
@@ -199,27 +181,6 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     }
     
     func angleTo(lat1: Float64, lon1: Float64, lat2: Float64, lon2: Float64) -> Float64 {
-//        let pi = 3.14159
-//        var angle = atan2(lat2 - lat1, lon2 - lon1) * 180/pi
-//        
-//        if (angle < 0){
-//            angle += 360
-//        }
-//        
-//        return angle + 90;
-        
-        print(lat1, lon1, lat2, lon2)
-        
-//        let φ1 = toRadians(lat1)
-//        let φ2 = toRadians(lat2)
-//        let λ1 = toRadians(lon1)
-//        let λ2 = toRadians(lon2)
-//        
-//        let y = sin(λ2-λ1) * cos(φ2)
-//        let x = cos(φ1)*sin(φ2)-sin(φ1)*cos(φ2)*cos(λ2-λ1)
-//        
-//        return toDegrees(atan2(y, x))
-        
         var angle = toDegrees(atan2(sin(toRadians(lon2-lon1))*cos(toRadians(lat2)), cos(toRadians(lat1))*sin(toRadians(lat2))-sin(toRadians(lat1))*cos(toRadians(lat2))*cos(toRadians(lon2-lon1))))
         
         if angle < 0 {
@@ -233,7 +194,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         let φ1 = toRadians(lat1)
         let φ2 = toRadians(lat2)
         let Δλ = toRadians(lon2-lon1)
-        let R = 6371000.0 // gives d in metres
+        let R = 6371000.0 // gives distance in meters
         let distance = acos(sin(φ1)*sin(φ2) + cos(φ1)*cos(φ2) * cos(Δλ) ) * R;
         
         return distance
